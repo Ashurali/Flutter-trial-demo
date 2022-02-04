79bd1c426697343778597f75f803839ea160e423
Michael <michaelks540@gmail.com>
2022-02-04 16:09:09 +0700

    Added assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -9,23 +11,56 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello Morgan'),
+        title: Text('Fluttery'),
         centerTitle: true,
-        backgroundColor: Colors.cyan[900],
+        backgroundColor: Colors.amber[900],
       ),
-      body: const Center(
-        child: Text("This is body!",
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              fontFamily: 'Righteous',
-            )),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.amber[900],
+                child: Center(child: Text('1'), heightFactor: 1)
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.cyan[400],
+                child: Center(child: Text('2'), heightFactor: 1)
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.red[400],
+                child: Align(child: Text('3'), alignment: Alignment.centerRight, heightFactor: 1,)
+            ),
+          ),
+        ],
       ),
+      /*Center(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.grey[400],
+            child: Image.asset(
+              'assets/sky_1.png',
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover
+            )
+          )
+      ),*/
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text('click'),
-        backgroundColor: Colors.cyan[900],
+        child: const Icon(
+          Icons.dashboard,
+        ),
+        backgroundColor: Colors.amber[900],
       ),
     );
   }
